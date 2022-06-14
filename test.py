62ffd5e037241e9f5e3c3a99255cb03925eb949b
EC2 Default User <ec2-user@ip-172-31-50-41.ec2.internal>
2022-06-14 09:07:37 +0000

    new code

---
+++ test.py
@@ -0,0 +1,13 @@
+#first_name = "Sulaiman"
+#surname = "Olubiyi"
+#print("My first name is {}. My family name is {}".format(first_name, surname))
+
+#my_int = 50
+#sentence = "The total comes to: "
+
+#print(sentence + (my_int)()
+
+# TypeError: can only concatenate str (not "int") to str
+
+string = "This is a string over\nthree lines\n\n\twith the third line indented"
+print(string)
